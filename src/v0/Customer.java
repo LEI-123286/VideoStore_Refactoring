c8f346980c12815171b06f49d74d87adcaf93fac
Pedro Brito <pedrobranco05@gmail.com>
2025-10-30 19:58:27 +0000

    V3

--- src/v0/Customer.java
+++ src/v0/Customer.java
@@ -29,39 +29,14 @@ public class Customer
 
 		// header
 		String result = "Rental Record for " + getName() + "\n";
-		
+
 		for (Rental each: _rentals)
 		{
-			double thisAmount = 0;
-
-			// determine amounts for each line
-			switch (each.getMovie().getPriceCode())
-			{
-				case REGULAR:
-					thisAmount += 2;
-					if (each.getDaysRented() > 2)
-						thisAmount += (each.getDaysRented() - 2) * 1.5;
-					break;
-				case NEW_RELEASE:
-					thisAmount += each.getDaysRented() * 3;
-					break;
-				case CHILDRENS:
-					thisAmount += 1.5;
-					if (each.getDaysRented() > 3)
-						thisAmount += (each.getDaysRented() - 3) * 1.5;
-					break;
-			}
-
-			// add frequent renter points
-			frequentRenterPoints++;
-
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.Code.NEW_RELEASE) && each.getDaysRented() > 1)
-				frequentRenterPoints++;
+			frequentRenterPoints += getFrequentRentalPoints(each);
 
 			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount + "\n";
-			totalAmount += thisAmount;
+			result += "\t" + each.getMovie().getTitle() + "\t" + each.getAmount() + "\n";
+			totalAmount += each.getAmount();
 		}
 
 		// add footer lines
@@ -70,5 +45,8 @@ public class Customer
 		return result;
 	}
 
-
+	public int getFrequentRentalPoints(Rental each)
+	{
+		return ((each.getMovie().getPriceCode() == Movie.Code.NEW_RELEASE) && each.getDaysRented() > 1) ? 2 : 1;
+	}
 }
